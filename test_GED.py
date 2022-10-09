a64806f847cb2c5ff7dc375bcf4a8a70da1b2302
Parth Sangle <pratiksangle6@gmail.com>
2022-10-08 23:59:29 -0400

    Added test cases

--- test_GED.py
+++ test_GED.py
@@ -1,3 +1,5 @@
+
+# test file starts
 import unittest
 import Read_Process_GED as GEDcode
 
@@ -14,3 +16,35 @@ class TestGED (unittest.TestCase):
 
 if __name__ == "__main__":
     unittest.main()
+
+
+# test file ends
+
+
+# US33 Sprint 1 code starts:
+def isIDUnique(id, list_id):
+    if id not in list_id:
+        return True
+    else:
+        return False
+
+    # if l_strTag == "INDI":
+    #                 l_strId = l_strArgs
+    #                 l_strName = extractNameFromLine(next(l_inpFile))
+    #                 if l_strId not in m_dictIndi and isIDUnique(l_strId, arrIds):
+    #                     m_dictIndi[l_strId] = l_strName.strip()
+    #                     arrIds.append(l_strId)
+    #             elif l_strTag == "FAM":
+    #                 l_strFamId = l_strArgs
+    #                 while True:
+    #                     l_strType, l_strId = extractSpouseIDFromLine(
+    #                         next(l_inpFile).strip())
+    #                     if (l_strType == "HUSB" or "WIFE") and l_strType != "":
+    #                         if l_strFamId not in m_dictFam and and isIDUnique(l_strId, arrIds):
+    #                             m_dictFam[l_strFamId] = {}
+#                                 arrIds.append(l_strId)
+    #                         m_dictFam[l_strFamId][l_strType] = l_strId
+    #                     else:
+    #                         break
+
+# US33 Sprint 1 code ends:
